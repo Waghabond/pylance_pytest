cbfe29bcc1863c81138182cf0992b8d519b88668
Rohit Wagh <roy@incub8.com.au>
2024-11-28 13:46:58 +1100

    minimal reproduction

---
+++ README.md
@@ -0,0 +1,1 @@
+Minimal reproduction of autocomplete bug in pytest test_* functions.
---
+++ tests/test_pylance_pytest.py
@@ -0,0 +1,8 @@
+def hi():
+    my_variable = None
+    my_variable
+
+
+def test_hi():
+    my_variable = None
+    my_variable
